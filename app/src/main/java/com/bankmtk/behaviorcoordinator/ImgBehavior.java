6dabe562859a2c60350c9efa6e80138a2b4f4c98
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-22 22:43:00 +0300

    correct field

--- app/src/main/java/com/bankmtk/behaviorcoordinator/ImgBehavior.java
+++ app/src/main/java/com/bankmtk/behaviorcoordinator/ImgBehavior.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.appbar.AppBarLayout;
